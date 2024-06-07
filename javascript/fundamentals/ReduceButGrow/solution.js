98db43b1a7f23e4b34875bf65f1312ce13f5488e
laloqwerty <lalowq.dev0109@gmail.com>
2024-06-07 11:37:02 -0600

    Added ReduceButGrow exercise

--- javascript/fundamentals/ReduceButGrow/solution.js
+++ javascript/fundamentals/ReduceButGrow/solution.js
@@ -1,11 +1,10 @@
 function grow(array){
-  
+    //asigns rsult to the first value of the array
     let result = array[0];
-
+    //iterates array
     for(let i=0; i<array.length-1; i++){
-    
+        //multiplies the array numbers 
         result = result * array[i+1];
-
     }    
     return result;
 }
